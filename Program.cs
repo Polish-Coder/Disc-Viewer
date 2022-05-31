cc87d85c299edb64c533b2cced2a4c9ad2e47ffd
Polygons Games Studio <polygonssoftware@gmail.com>
2022-05-31 21:42:09 +0200

    Added reading a directory

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Disc Viewer!\nPlease enter folder directory");
+
+string directory = Console.ReadLine();
+
+if (Directory.Exists(directory))
+{
+    Console.WriteLine(directory + " exists");
+}
+else
+{
+    Console.WriteLine(directory + " does not exists");
+}
