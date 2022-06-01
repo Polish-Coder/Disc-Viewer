ec9aaa7eaf4c4a5958d4db3e39cb53d677fd6824
Polygons Games Studio <polygonssoftware@gmail.com>
2022-06-01 14:43:49 +0200

    Added Name value in FileObject class

--- src/scripts/FileObject.cs
+++ src/scripts/FileObject.cs
@@ -3,13 +3,15 @@
     public class FileObject
     {
         public object Info;
+        public string Name;
         public string Directory;
         public float SizeInBytes;
         public string Size;
 
-        public FileObject(object info, string directory, float sizeInBytes, string size)
+        public FileObject(object info, string name, string directory, float sizeInBytes, string size)
         {
             Info = info;
+            Name = name;
             Directory = directory;
             SizeInBytes = sizeInBytes;
             Size = size;
